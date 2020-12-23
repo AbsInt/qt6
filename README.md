a09fc6ad624fab900be0d30d296675ee9b40f8ba
Christoph Cullmann <cullmann@kde.org>
2020-12-23 15:55:06 +0100

    add update scripty to pull stuff directly from qt git, bug 30132

--- README.md
+++ README.md
@@ -4,7 +4,10 @@ This Git contains the Qt 6.x sources.
 
 # How to update
 
-* delete old src
-* unpack a new qt-everywhere-src-6.XXXXXXX.tar.xz
-* move the unpacked content to src
-* add stuff to Git
+Run
+
+./update <version>
+
+e.g.
+
+./update 6.0
---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# get fresh toplevel module with right branch
+git clone --single-branch --branch "$1" http://code.qt.io/qt/qt5.git || exit 1
+
+cd qt5 || exit 1
+
+# get needed sub-modules
+perl init-repository -f --module-subset=qt5compat,qtbase,qtdeclarative,qtsvg,qttools,qtwayland || exit 1
+
+# kill git files
+find . -name ".git" -exec rm -rf --one-file-system {} \;
+find . -name ".gitignore" -exec rm -f {} \;
+find . -name ".gitattributes" -exec rm -f {} \;
+
+# replace old stuff
+cd .. || exit 1
+rm -rf src || exit 1
+mv qt5 src || exit 1
